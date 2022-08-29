9303bafddaa1638051d3bbc72f0cf5c8d63ccb61
DrDonkeyKong <davidkling@yahoo.com>
2022-08-29 19:08:08 +0000

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,5 +1,5 @@
 import unittest
-import translator
+from .. import translator
 
 class TestTranslatorString(unittest.TestCase):
 
--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,4 @@
-from machinetranslation import translator
+import machinetranslation as mt
 from flask import Flask, render_template, request
 import json
 
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translation = mt.translator.englishToFrench(textToTranslate)
+    return translation
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translation = mt.translator.frenchToEnglish(textToTranslate)
+    return translation
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
